b9faaae3c1b887713d0916255090130648abaf52
IsmailSaleekh <ismailsalikhodjaev@gmail.com>
2018-04-22 22:01:59 +0500

    feat: added new refferences into sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { ListPage } from '../pages/list/list';
 import { TabsPage } from '../pages/tabs/tabs';
 import { CartPage } from '../pages/cart/cart';
+import { TypeListPage } from '../pages/type-list/type-list';
 
 @Component({
   templateUrl: 'app.html'
@@ -15,33 +15,33 @@ export class MyApp {
 
   rootPage: any = TabsPage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
-    // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: TabsPage },
-      { title: 'List', component: ListPage },
-      { title: 'Cart', component: CartPage},
-      { title: 'Genres', component: TabsPage},
-      {title: 'About', component: TabsPage}
+      { title: 'Cart', component: CartPage },
+      { title: 'Genres', component: TypeListPage },
+      { title: 'Authors', component: TypeListPage },
+      { title: 'About', component: TabsPage }
     ];
   }
 
-  initializeApp() { 
+  initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
   }
 
   openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    if (page.title === 'Genres' || page.title === "Authors") {
+      this.nav.setRoot(page.component, { type: page.title });
+    } else {
+      this.nav.setRoot(page.component)
+    }
   }
 }
