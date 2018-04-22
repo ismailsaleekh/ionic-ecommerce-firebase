6d0d6e3f0e02b26e7949a44cb72d423a45eed8e5
IsmailSaleekh <ismailsalikhodjaev@gmail.com>
2018-04-22 22:08:32 +0500

    feat: product view improved, add to favorites added

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CartProvider } from '../../providers/cart/cart';
+import { FavoritesProvider } from '../../providers/favorites/favorites';
 
 
 @IonicPage()
@@ -10,17 +11,31 @@ import { CartProvider } from '../../providers/cart/cart';
 })
 export class ProductPage {
 
-  product: {} = {}
+  product: any = {}
+  inFav: boolean = false
 
   constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public cart: CartProvider
-            ) {
+    public navParams: NavParams,
+    public cart: CartProvider,
+    public favoritesProvider: FavoritesProvider
+  ) { }
+
+  async ionViewDidLoad() {
+    this.product = this.navParams.data
+
+    const list = await this.favoritesProvider.getFromStorage()
+
+    const index = list.findIndex(item => {
+      return item.key === this.product.key
+    })
+
+    if (index !== -1) {
+      this.inFav = true
+    }
   }
 
-  ionViewDidLoad() {
-    this.product = this.cart.selectedProduct
+  async addToFavs(product) {
+    await this.favoritesProvider.addToFav(product)
+    this.inFav = true
   }
-
-
 }
